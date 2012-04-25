8be3d4d578814a639c02783d12718a2530617f6d
roc <rcrmn16@gmail.com>
2012-04-25 16:18:12 +0200

    Initial Commit + Project Files

---
+++ DeltaPL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DeltaPL
+//
+//  Created by Roc Ramon Currius on 24/04/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
